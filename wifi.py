26b86ef13d6989224f1906f3c960433c2ee50557
Ron van Wier <ron@van-wier.org>
2020-07-09 19:00:54 +0200

    config used for wifi

--- wifi.py
+++ wifi.py
@@ -1,13 +1,15 @@
 import json
 import time
 import network
+import config
 
 def check():
     nic = network.WLAN(network.STA_IF)
     if not nic.isconnected():
         # enable station interface and connect to WiFi access point
+        cfg = cfgRead("wifi")
         nic.active(True)
-        nic.connect('iot','iot1064wh')
+        nic.connect(cfg['ssid'],cfg['password'])
         while not nic.isconnected():
             print('not connected')
             time.sleep(1)
