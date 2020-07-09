31c846124d861716671ba2219ec1ba73362e7eee
Ron van Wier <ron@van-wier.org>
2020-07-09 19:11:40 +0200

    trmovr JSON import

--- wifi.py
+++ wifi.py
@@ -1,14 +1,13 @@
-import json
 import time
 import network
 import config
 
 def check():
     nic = network.WLAN(network.STA_IF)
+    nic.active(True)
     if not nic.isconnected():
         # enable station interface and connect to WiFi access point
         cfg = cfgRead("wifi")
-        nic.active(True)
         nic.connect(cfg['ssid'],cfg['password'])
         while not nic.isconnected():
             print('not connected')
